c669fee7aeb33a96e991d3a24f38cbb7dcfecb54
John Doe <johndoe@example.com>
2019-06-27 21:22:23 +0300

    create templates

--- blog/models.py
+++ blog/models.py
@@ -11,9 +11,11 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     updated_on = models.DateTimeField(auto_now=True)
-    created_on = models.DateTimeField(choices=STATUS)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
